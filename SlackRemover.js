9941ab4264ff2f991574a759b1e9d2e13ee9aeb2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-05-25 21:54:36 +0900

    SlackAPI雛形作成

--- SlackRemover.js
+++ SlackRemover.js
@@ -1,3 +1,14 @@
-function myFunction() {
-  
+function getFileList() {
+    var params = {
+        'token': "",
+        'channel': "",
+        'count': ""
+    }
+    var options = {
+        'method': 'GET',
+        'payload': params
+    }
+
+    var url = "https://slack.com/api/files.list"
+    UrlFetchApp.fetch(url, options)
 }
